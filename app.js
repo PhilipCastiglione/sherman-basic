335d8f74e7a319eb6dfe96dd2d3d41c36f72acd7
Philip Castiglione <philipcastiglione@gmail.com>
2015-06-08 21:27:34 +1000

    refactored

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
 
+  // set tile set
+  var tile = {
+    B: '<div class="blank"></div>',
+    W: '<div class="wall"></div>',
+    O: '<div class="obstacle"></div>',
+    G: '<div class="goal"></div>',
+    P: '<div class="player"></div>',
+    T: '<div class="thing"></div>'
+  };
+
+  // set board
   var board = {
     numTiles: 6,
     tiles: [['B','B','W','B','B','B'],['B','B','W','B','W','B'],['W','B','W','B','W','B'],['W','B','W','B','O','B'],['B','B','B','B','B','W'],['O','B','W','B','B','W']],
@@ -17,20 +28,6 @@ $(document).ready(function() {
     }
   };
 
-  var tile = {
-    B: '<div class="blank"></div>',
-    W: '<div class="wall"></div>',
-    O: '<div class="obstacle"></div>',
-    G: '<div class="goal"></div>',
-    P: '<div class="player"></div>',
-    T: '<div class="thing"></div>'
-  };
-
-  function compare(boardProperty1, boardProperty2) {
-    return (board[boardProperty1].row === board[boardProperty2].row &&
-      board[boardProperty1].col === board[boardProperty2].col)
-  }
-
   function renderBoard() {
     var $board = $('<div id="board">');
     // for each row
@@ -38,15 +35,15 @@ $(document).ready(function() {
       var $row = $('<div class="row">');
       // for each tile
       _.each(row, function(content, colNum, list2) {
-        // display if player
+        // display, if player
         if (rowNum + 1 === board.playerTile.row &&
          colNum + 1 === board.playerTile.col) {
           $row.append($('<div class="tile">').html(tile['P']));
-        // display if thing
+        // display, if thing
         } else if (rowNum + 1 === board.thingTile.row &&
          colNum + 1 === board.thingTile.col) {
           $row.append($('<div class="tile">').html(tile['T']));
-        // display if goal
+        // display, if goal
         } else if (rowNum + 1 === board.goalTile.row &&
          colNum + 1 === board.goalTile.col) {
           $row.append($('<div class="tile">').html(tile['G']));
@@ -62,121 +59,104 @@ $(document).ready(function() {
     $('#board-wrapper').html($board);
   }
 
-  function moveItem(direction, boardProp) {
-    var itemTile = board[boardProp];
-    var destinationTile;
+  function moveItem(direction, boardItem) {
+    // points to item tile location
+    var itemTile = board[boardItem];
+
+    // returns current type of destination tile - N means none
+    function setDestinationTile(rowOffset, colOffset) {
+      try {
+        var tile = board.tiles[itemTile.row - rowOffset][itemTile.col - colOffset] || 'N';
+      } catch(e) {
+        var tile = 'N';
+      }
+      return tile;
+    }
+
+    // check if the move is valid
+    function canMove(limitingEdge, limitingVal) {
+      if (itemTile[limitingEdge] !== limitingVal &&
+         destinationTile !== 'W') {
+        if (boardItem !== 'thingTile' ||
+          destinationTile !== 'O') {
+          return true;
+        }
+      }
+      return false;
+    }
+
+    // for arrow keys: set destination tile, check validity, move if valid then increment move count
     switch(direction) {
     case 37: // left
-      try {
-        destinationTile = board.tiles[itemTile.row - 1][itemTile.col - 2];
-      } catch(e) {
-        destinationTile = 'N';
-      }
-
-      if (itemTile.col !== 1 &&
-         destinationTile !== 'W') {
-        if (boardProp !== 'thingTile' ||
-          destinationTile !== 'O') {
-          itemTile.col--;
-        }
-      }
-
-      if (boardProp === 'playerTile') {
-        incrementMoves();
-      }
-
+      var destinationTile = setDestinationTile(1, 2);
+      if (canMove('col', 1)) itemTile.col--;
+      if (boardItem === 'playerTile') incrementMoves();
       break;
     case 38: // up
-      try {
-        destinationTile = board.tiles[itemTile.row - 2][itemTile.col - 1];
-      } catch(e) {
-        destinationTile = 'N';
-      }
-        
-      if (itemTile.row !== 1 &&
-        destinationTile !== 'W') {
-        if (boardProp !== 'thingTile' ||
-          destinationTile !== 'O') {
-          itemTile.row--;
-        }
-      }
-      
-      if (boardProp === 'playerTile') {
-        incrementMoves();
-      }
-
+      var destinationTile = setDestinationTile(2, 1);
+      if (canMove('row', 1)) itemTile.row--;
+      if (boardItem === 'playerTile') incrementMoves();
       break;
     case 39: // right
-      try {
-        destinationTile = board.tiles[itemTile.row - 1][itemTile.col];
-      } catch(e) {
-        destinationTile = 'N';
-      }
-
-      if (itemTile.col !== board.numTiles &&
-         destinationTile !== 'W') {
-        if (boardProp !== 'thingTile' ||
-          destinationTile !== 'O') {
-          itemTile.col++;
-        }
-      }
-      
-      if (boardProp === 'playerTile') {
-        incrementMoves();
-      }
-        
+      var destinationTile = setDestinationTile(1, 0);
+      if (canMove('col', board.numTiles)) itemTile.col++;
+      if (boardItem === 'playerTile') incrementMoves();
       break;
     case 40: // down
-      try {
-        destinationTile = board.tiles[itemTile.row][itemTile.col - 1];
-      } catch(e) {
-        destinationTile = 'N';
-      }
-
-      if (itemTile.row !== board.numTiles &&
-        destinationTile !== 'W') {
-        if (boardProp !== 'thingTile' ||
-          destinationTile !== 'O') {
-          itemTile.row++;
-        }
-      }
-      
-      if (boardProp === 'playerTile') {
-        incrementMoves();
-      }
-
+      var destinationTile = setDestinationTile(0, 1);
+      if (canMove('row', board.numTiles)) itemTile.row++;
+      if (boardItem === 'playerTile') incrementMoves();
       break;
     default:
       break;
     }
   }
 
+  // checks if location of two board items is the same
+  function compare(boardItem1, boardItem2) {
+    return (board[boardItem1].row === board[boardItem2].row &&
+      board[boardItem1].col === board[boardItem2].col)
+  }
+
+  // on win, displays message, stops game and checks for new best score
   function checkWin() {
     if (compare('thingTile','goalTile')) {
-      var $h1 = $('<h1>').text('you win');
-      $('body').append($h1);
+      $('body').append($('<h1>').text('you win'));
       $(document).off('keyup', runMove);
-
-      if (parseInt($('#moves').text(),10) < parseInt(localStorage.getItem('bestScore'),10)) {
-        localStorage.setItem('bestScore', parseInt($('#moves').text(),10));
-        $('#best').text(localStorage.getItem('bestScore'));
-      }
+      checkNewBestScore();
     }
   }
 
+  // checks new best score and sets if valid
+  function checkNewBestScore() {
+    if (parseInt($('#moves').text(),10) < parseInt(localStorage.getItem('bestScore'),10)) {
+      localStorage.setItem('bestScore', parseInt($('#moves').text(),10));
+      $('#best').text(localStorage.getItem('bestScore'));
+    }
+  }
+
+  // increments the current move counter and displays
   function incrementMoves() {
     var count = parseInt($('#moves').text(),10);
     $('#moves').text(++count);
   }
 
+  // run move on event
   function runMove() {
+    // move player
     moveItem(event.which, 'playerTile');
+    // if push
     if (compare('playerTile', 'thingTile')) {
+      // move thing
       moveItem(event.which, 'thingTile');
+      // if thing didn't move (ie. wall or object obstructs)
       if (compare('playerTile', 'thingTile')) {
         var direction = (event.which - 35) % 4 + 37; // reverses direction
+        // leave player and put thing where player was (ie. swap positions)
         moveItem(direction, 'thingTile');
+        // if thing still didn't move
         if (compare('playerTile', 'thingTile')) {
+          // swap was invalid, return player to starting position
           moveItem(direction, 'playerTile');
         }
       }
@@ -185,12 +165,15 @@ $(document).ready(function() {
     renderBoard();
   }
 
-
+  // if first time user and no best score, set to 100
   if (localStorage.getItem('bestScore') === null) {
     localStorage.setItem('bestScore', 100);
   }
+  // put best score to screen
   $('#best').text(localStorage.getItem('bestScore'));
+  // initial board render
   renderBoard();
+  // establish event listener
   $(document).on('keyup', runMove);
 
 });
